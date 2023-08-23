1a0feef8b15e42393a8fef9a32545f3ca90f7382
Ryo ikegata <ryo.ike.6153@gmail.com>
2023-08-23 23:33:22 +0900

    Add:brezzeのReact導入

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
 });
